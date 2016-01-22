38012eac4557c10ca2dd8cd506e32b5f5fe95a8c
yetzt <git@yetzt.me>
2016-01-22 20:08:40 +0100

    tweet about new lifts.

--- liftbot.js
+++ liftbot.js
@@ -8,6 +8,7 @@ var path = require("path");
 var debug = require("debug")("liftbot");
 var ws = require("ws");
 var twitter = require("twitter");
+var request = require("request");
 
 // check for config file
 if (!fs.existsSync(path.resolve(__dirname, "config.js"))) console.error("config.js not found") || process.exit(1);
@@ -17,6 +18,31 @@ var config = require(path.resolve(__dirname, "config.js"));
 
 // load station data
 var stations = JSON.parse(fs.readFileSync(path.resolve(__dirname, "data/stations.json")));
+
+// load known lifts
+var knownlifts = [];
+if (fs.existsSync(path.resolve(__dirname, "data/knownlifts.json"))) {
+	knownlifts = JSON.parse(fs.readFileSync(path.resolve(__dirname, "data/knownlifts.json")));
+} else {
+	request({
+		method: "GET",
+		url: "https://adam.noncd.db.de/api/v1.0/facilities",
+		headers: { "user-agent": "liftbot/1" }
+	}, function(err, resp, data){
+		if (err) return debug("error fetching lifts: %s", err);
+		if (resp.statusCode !== 200) return debug("error fetching lifts: status code %d", resp.statusCode);
+		try {
+			data = JSON.parse(data);
+		} catch(err) {
+			return debug("error parsing lifts: %s", err);
+		}
+		data.forEach(function(d){
+			knownlifts.push(d.equipmentnumber);
+		});
+		fs.writeFile(path.resolve(__dirname, "data/knownlifts.json"), JSON.stringify(knownlifts));
+		debug("got %d known lifts", knownlifts.length);
+	});
+}
 
 // create twitter client
 var t = new twitter(config.twitter);
@@ -33,9 +59,16 @@ new ws(config.websocket).on("open", function(){
 	if (data.equipmentnumber === 0) return;
 
 	debug("incoming data: %j", data);
-
+	
 	// build message
 	var message = [];
+
+	// check for new lift
+	if (knownlifts.indexOf(data.equipmentnumber) < 0) {
+		message.push("Neuer Aufzug:");
+		knownlifts.push(data.equipmentnumber);
+		fs.writeFile(path.resolve(__dirname, "data/knownlifts.json"), JSON.stringify(knownlifts));
+	} 
 
 	if (stations.hasOwnProperty(data.stationnumber.toString())) {
 		message.push(stations[data.stationnumber.toString()]+",");
